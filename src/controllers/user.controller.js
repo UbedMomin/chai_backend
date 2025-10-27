d6c2019e15eb2915fc0fbea5d7908383ccdbaed3
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-10-27 19:55:36 +0530

    Complete loging code with errors Lect 16

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,25 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefereshTokens = async (userId) => {
+  try {
+   const user = await User,findById(userId)
+  const accessToken = user.generateAccessToken()
+  const refreshToken =  user.generateRefreshToken()
+
+user.refreshToken = refreshToken
+await user.save({validateBeforeSave:false})
+
+return {accessToken, refreshToken}
+
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh and access token"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
   console.log("email", email);
@@ -26,11 +45,35 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const isPasswordValid = await user.isPasswordCorrect(password);
- 
+
   if (!isPasswordValid) {
     throw new ApiError(401, "Invalid User Credentials");
   }
 
+const {accessToken, refreshToken} =  await generateAccessAndRefereshTokens(user._id)
+
+const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+const options = {
+  httpOnly: true,
+  secure: true
+}
+
+return res.status(200).cookie("accessToken", accessToken, options).cookie("refreshToken", refreshToken, options).json(
+  new ApiResponse(
+    200,
+    {
+      user: loggedInUser, accessToken, refreshToken
+    },
+    "User logged in successfully"
+
+  )
+
+)
+
+const logoutUser = asyncHandler(async (req, res) =>{
+  
+})
   // console.log(req.files);
   // ✅ FIXED: Corrected typo "avatarLoaclPath" to "avatarLocalPath"
   const avatarLocalPath = req.files?.avatar?.[0]?.path;
