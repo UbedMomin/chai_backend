c9d39c3a3d35c9284acad3b50c72c520686e6574
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-11-03 23:59:14 +0530

    error corrected

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -159,51 +159,55 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
 
- const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
-
- if (!incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "unauthorized access - no refresh token provided");
-} 
- 
- try {
-  const decodedToken = jwt.verify(
-   incomingRefreshToken,
-   process.env.REFRESH_TOKEN_SECRET,
- )
- 
- const user = await User.findById(decodedToken?._id)
- if (!user) {
-     throw new ApiError(401, "Invalid refresh token - user not found");
- } 
- 
- if(incomingRefreshToken !== user?.refreshToken){
-     throw new ApiError(401, "Refresg Token is expired, please login again");
- }
- 
- const options = {
-   httpOnly: true,
-   secure: true,
- }
- 
- const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
- 
- return res
-   .status(200)  
-   .cookie("accessToken", accessToken, options)
-   .cookie("refreshToken", newRefreshToken, options)
-   .json(
-     new ApiResponse(
-       200,
-       { accessToken, refreshToken: newRefreshToken }, 
-       "Access token refreshed successfully"
-     )
-   );
- } catch (error) {
-  throw new ApiError(401, error?.message || "Invalid or expired refresh token"
- }
-} );
+  }
 
-export { registerUser, loginUser, logoutUser , refreshAccessToken };
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token - user not found");
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresg Token is expired, please login again");
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefereshTokens(user._id);
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access token refreshed successfully"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(
+      401,
+      error?.message || "Invalid or expired refresh token"
+    );
+  }
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
 
 // $ sign these are operators of mongodb
