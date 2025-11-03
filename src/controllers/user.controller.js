b43f50cb592b1d99729652adbca529452d4c8432
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-11-03 23:58:16 +0530

    add refresh accessToken endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -204,6 +204,6 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
  }
 } );
 
-export { registerUser, loginUser, logoutUser };
+export { registerUser, loginUser, logoutUser , refreshAccessToken };
 
 // $ sign these are operators of mongodb
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"; // ✅ fixed import
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,9 +24,8 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken); // To be implemented
 
 export default router;
 
-
-//fix the login route in postman request check it tommrow and fix it 
-
+//fix the login route in postman request check it tommrow and fix it
