38e417c7a155bc7c6cf3eb57ba35cd1e451b53c3
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-11-05 23:14:52 +0530

    refactor: define SubscriptionSchema with subscriber and channel fields

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,7 +1,17 @@
 import mongoose, {Schema} from "mongoose"; 
 
 
-const SubscriptionSchema = new Schema({})
+const SubscriptionSchema = new Schema({
+    subscriber:{
+        type: Schema.Types.ObjectId, // one who is subscribing
+        ref: "User",
+
+    },
+    channel:{
+        type: Schema.Types.ObjectId, // one who is being subscribed to 
+        ref:"User",
+    }
+}, {timestamps: true});
 
 
 
