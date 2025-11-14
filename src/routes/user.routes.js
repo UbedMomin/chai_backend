3cb9d2bd03f75daa138aaee5be7d73a2edcdfe8d
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-11-14 17:19:16 +0530

    feat: add secured routes for changing password, getting current user, and updating account details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"; // ✅ fixed import
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -25,6 +32,21 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken); // To be implemented
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route(".curreny-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(
+  verifyJWT,
+  upload.single("coverImage"),
+  updateUserCoverImage
+);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
 
